83e0ee5e8bdbfc678c409bbeb9c116f3f9656722
Taylor Quas <tquas24@gmail.com>
2024-08-29 16:46:29 -0400

    m

--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/RestTransferTypeService.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/RestTransferTypeService.java
@@ -1,16 +1,53 @@
 package com.techelevator.tenmo.services;
 
 import com.techelevator.tenmo.model.AuthenticatedUser;
+import com.techelevator.tenmo.model.TransferStatus;
 import com.techelevator.tenmo.model.TransferType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 public class RestTransferTypeService implements TransferTypeService{
+    private final String API_BASE_URL;
+    private RestTemplate restTemplate = new RestTemplate();
+    public RestTransferTypeService(String API_BASE_URL) {
+        this.API_BASE_URL = API_BASE_URL;
+    }
     @Override
     public TransferType getTransferType(AuthenticatedUser authenticatedUser, String description) {
-        return null;
+        TransferType transferType = null;
+        try {
+            String url = API_BASE_URL + "/transfertype/sort?description=" + description;
+            transferType = restTemplate.exchange(url, HttpMethod.GET, makeEntity(authenticatedUser),
+                    TransferType.class).getBody();
+        } catch (ResourceAccessException e) {
+            System.out.println("Error in resource access: " + e.getMessage());
+        } catch (RestClientResponseException e) {
+            System.out.println("API error - status code: " + e.getRawStatusCode() + ", Error message: " + e.getMessage());
+        }
+        return transferType;
     }
-
     @Override
-    public TransferType getTransferTypeById(AuthenticatedUser authenticatedUser, int id) {
-        return null;
+    public TransferType getTransferTypeById(AuthenticatedUser authenticatedUser, int transferTypeId) {
+        TransferType transferType = null;
+        try {
+            String url = API_BASE_URL + "/transfertype/" + transferTypeId;
+            transferType = restTemplate.exchange(url, HttpMethod.GET, makeEntity(authenticatedUser),
+                    TransferType.class).getBody();
+        } catch (ResourceAccessException e) {
+            System.out.println("Error in resource access: " + e.getMessage());
+        } catch (RestClientResponseException e) {
+            System.out.println("API error - status code: " + e.getRawStatusCode() + ", Error message: " + e.getMessage());
+        }
+        return transferType;
+    }
+    private HttpEntity makeEntity(AuthenticatedUser authenticatedUser) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(authenticatedUser.getToken());
+        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        return httpEntity;
     }
 }
